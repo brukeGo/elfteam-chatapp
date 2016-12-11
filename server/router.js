2bbe35eb273e89f53dd618b93818e0fab0acf257
Holly Dinh <dinh.holly@gmail.com>
2016-12-11 15:06:29 -0800

    add group chat reject

--- server/router.js
+++ server/router.js
@@ -87,10 +87,30 @@ router.post('/msg', (req, res, next) => {
   var dat = {
     rec: req.body.rec,
     msg: req.body.msg,
-    files: req.body.files,
     time: req.body.time
   };
   auth.handle_msg(req.decod.sub, dat, (er) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({info: 'ok'});
+    }
+  });
+});
+
+router.post('/g_msg', (req, res, next) => {
+  var dat;
+  try {  
+    dat = {
+      name: req.body.gname,
+      msg: req.body.msg,
+      members: JSON.parse(req.body.gmembers),
+      time: req.body.time
+    };
+  } catch(er) {
+    res.json({err: er.message});
+  }
+  auth.handle_group_msg(req.decod.sub, dat, (er) => {
     if (er) {
       res.json({err: er.message});
     } else {
@@ -104,14 +124,104 @@ router.post('/unread', (req, res, next) => {
     if (er) {
       res.json({err: er.message});
     } else {
-      res.json({unread: unread});
+      auth.clear_unread(req.decod.sub, (er) => {
+        if (er) {
+          res.json({err: er.message});
+        } else {
+          res.json({unread: unread});
+        }
+      });
     }
   });
 });
 
-router.post('/logout', (req, res, next) => {
-  auth.logout(req.decod, () => {
-    res.json({err: null});
+router.post('/check_gchat', (req, res, next) => {
+  auth.check_gchat(req.body.gname, (er) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({info: 'ok'});
+    }
+  });
+});
+
+router.post('/create_gchat', (req, res, next) => {
+  var dat;
+  try {  
+    dat = {
+      name: req.body.gname,
+      key: req.body.gkey,
+      members: JSON.parse(req.body.gmembers),
+      sig: req.body.sig
+    };
+  } catch(er) {
+    res.json({err: er.message});
+  }
+  auth.create_gchat(req.decod.sub, dat, (er) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({info: 'ok'});
+    }
+  });
+});
+
+router.post('/del_gchat', (req, res, next) => {
+  var dat;
+  try {  
+    dat = {
+      name: req.body.gname,
+      members: JSON.parse(req.body.gmembers)
+    };
+  } catch(er) {
+    res.json({err: er.message});
+  }
+  auth.del_gchat(req.decod.sub, dat.name, dat.members, (er) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({info: 'ok'});
+    }
+  });
+});
+
+router.post('/fetch_gchat_del', (req, res, next) => {
+  auth.fetch_gchat_del(req.decod.sub, (er, del) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({gchat_del: del});
+    }
+  });
+});
+
+router.post('/fetch_gchat_req', (req, res, next) => {
+  auth.fetch_gchat_req(req.decod.sub, (er, req) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({gchat_req: req});
+    }
+  });
+});
+
+router.post('/fetch_gchat_rej', (req, res, next) => {
+  auth.fetch_gchat_rej(req.decod.sub, (er, rej) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({gchat_rej: rej});
+    }
+  });
+});
+
+router.post('/send_gchat_rej', (req, res, next) => {
+  auth.send_gchat_rej(req.decod.sub, req.body.dat, (er) => {
+    if (er) {
+      res.json({err: er.message});
+    } else {
+      res.json({info: 'ok'});
+    }
   });
 });
 
